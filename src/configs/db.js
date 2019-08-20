172504f3f9e68d0070ee2f938d15a45f4d3cda1e
Léo Castro <leonardo_carvalho@outlook.com>
2019-08-19 22:48:05 -0300

    Fix url db connection

--- src/configs/db.js
+++ src/configs/db.js
@@ -8,6 +8,7 @@ export default () => {
     poolSize: 10, // Maintain up to 10 socket connections
     // If not connected, return errors immediately rather than waiting for reconnect
     bufferMaxEntries: 0,
+    useNewUrlParser: true,
   };
 
   const db = mongoose.connect(process.env.DB_URL, options).then(() => {
